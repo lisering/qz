f15247f84dad81d104f7b664c1459dd637616580
宋方华 <songfh@sunits.com>
2017-10-18 13:36:11 +0800

    list add description

--- routes/order.js
+++ routes/order.js
@@ -5,9 +5,14 @@ var db = require('../data/db');
 router.get('/:orderby/:order', (req, res, next) => {
   var orderBy = req.params.orderby;
   var order = req.params.order;
-  let sql = `SELECT * FROM state ORDER BY CONVERT(${orderBy} USING GBK) ${order}`;
-  if (orderBy = 'stateImg') {
-    sql = `SELECT * FROM state ORDER BY ${orderBy} ${order}`;
+  var sql;
+  switch(orderBy) {
+    case 'id':
+    case  'stateImg':
+      sql = `SELECT * FROM state ORDER BY ${orderBy} ${order}`;
+      break;
+    default:
+      sql = `SELECT * FROM state ORDER BY CONVERT(${orderBy} USING GBK) ${order}`;
   }
   let query = db.query(sql, (err, result) => {
     if (err) {
