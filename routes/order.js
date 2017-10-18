6f3bb05a0ab25c67e5c29ce354ca118e8c07530a
宋方华 <songfh@sunits.com>
2017-10-18 11:26:40 +0800

    add filter search

--- routes/order.js
+++ routes/order.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 var db = require('../data/db');
 
-router.get('/', (req, res, next) => {
-  res.send('order');
+router.get('/:orderby/:order', (req, res, next) => {
+  var orderBy = req.params.orderby;
+  var order = req.params.order;
+  let sql = `SELECT * FROM state ORDER BY CONVERT(${orderBy} USING GBK) ${order}`;
+  if (orderBy = 'stateImg') {
+    sql = `SELECT * FROM state ORDER BY ${orderBy} ${order}`;
+  }
+  let query = db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(sql);
+    res.json(result);
+  });
 });
 
 module.exports = router;
